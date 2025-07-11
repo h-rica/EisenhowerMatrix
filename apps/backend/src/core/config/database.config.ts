2ae81696f5035f147dafc2d59d85e2cc47f83d27
Harivonjy Rica <contact.harivonjy@gmail.com>
2025-07-11 14:45:58 +0300

    feat(backend): implement DatabaseConfig with TypeORM integration for dynamic database configuration

--- apps/backend/src/core/config/database.config.ts
+++ apps/backend/src/core/config/database.config.ts
@@ -1,1 +1,25 @@
-export default () => ({})
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class DatabaseConfig implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get('DB_HOST', 'localhost'),
+      port: this.configService.get('DB_PORT', 5432),
+      username: this.configService.get('DB_USERNAME', 'postgres'),
+      password: this.configService.get('DB_PASSWORD', 'postgres'),
+      database: this.configService.get('DB_NAME', 'eisenhower_matrix_db'),
+      entities: [],
+      synchronize: this.configService.get('NODE_ENV') !== 'production',
+      logging: this.configService.get('NODE_ENV') === 'development',
+      migrations: ['dist/database/mirgations/*{.ts, .js}'],
+      migrationsRun: true,
+      ssl: this.configService.get('DB_SSL') === 'true' ? { rejectUnauthorized: false } : false,
+    }
+  }
+}
