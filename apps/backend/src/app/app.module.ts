f3f4033b208e130e7de2f66e1b9f84bf3bbec0d5
Harivonjy Rica <contact.harivonjy@gmail.com>
2025-07-11 14:37:09 +0300

    chore(backend): update config import to use default.config

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import configuration from '../core/config/configuration';
+import configuration from '../core/config/default.config';
 
 @Module({
   imports: [
