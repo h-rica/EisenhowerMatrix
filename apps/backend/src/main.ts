9e464c8e49fdea923db7bd470e467a2a63001e0f
Harivonjy Rica <contact.harivonjy@gmail.com>
2025-07-11 13:55:52 +0300

    feat(backend): add global validation pipe and set request validation rules

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
@@ -6,10 +6,21 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
   const configService = app.get(ConfigService);
   const port = configService.getOrThrow<number>('port');
-  app.setGlobalPrefix(globalPrefix);
+
+  // Global validation pipe
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
+
+  // Global prefix
+  const globalPrefix = 'api';
+  app.setGlobalPrefix('api');
 
   // CORS configuration
   app.enableCors({
