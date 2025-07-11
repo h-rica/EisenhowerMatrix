4b12d53d28b0cc00ece96973b9fd321a97fb30cd
Harivonjy Rica <contact.harivonjy@gmail.com>
2025-07-11 13:53:24 +0300

    feat(backend): integrate Swagger for API documentation

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,12 +1,8 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +17,16 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   })
+
+  // Swagger documentation
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Eisenhower Matrix API')
+    .setDescription('A comprehensive task management system based on the Eisenhower Matrix')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, documentConfig);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
