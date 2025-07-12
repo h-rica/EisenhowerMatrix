3fd122873d48810d4f2f486feb94cb868af18166
Harivonjy Rica <contact.harivonjy@gmail.com>
2025-07-12 14:40:30 +0300

    feat(backend): extend TaskPriority enum with detailed priority levels

--- apps/backend/src/core/enums/task-priority.enum.ts
+++ apps/backend/src/core/enums/task-priority.enum.ts
@@ -1,2 +1,6 @@
 export enum TaskPriority {
+  URGENT_IMPORTANT = 'urgent_important', // Do ti (Green)
+  NOT_URGENT_IMPORTANT = 'not_urgent_important', // Schedule it (Orange)
+  URGENT_NOT_IMPORTANT = 'urgent_not_important', // Delegate it (Yellow)
+  NOT_URGENT_NOT_IMPORTANT = 'not_urgent_not_important', // Delete it (Red)
 }
